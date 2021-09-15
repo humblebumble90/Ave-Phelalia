77acff2a332b9c5a39d5a388877736bfb493973d
humblebumble90 <hyngsuki@gmail.com>
2021-09-15 17:55:25 -0400

    Revised bgController

--- Assets/Asset/Scripts/BackgroundController.cs
+++ Assets/Asset/Scripts/BackgroundController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public GameObject bgImg;
-    public float resetPosX;
+    public GameObject bgImg1;
+    public GameObject bgImg2;
     public float speed;
-    private Vector3 pos;
+    private Vector3 pos1;
+    private Vector3 pos2;
     private Vector3 newPos;
+    private float resetPosX1;
+    private float resetPosX2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +24,17 @@ public class BackgroundController : MonoBehaviour
     {
         move();
     }
-    void move()
-    {
-        if (transform.position.x > resetPosX)
-        {
-            transform.position -= newPos;
-        }
-        else
-        {
-            transform.position = pos;
-            Debug.Log("Reset position");
-        }
-        Debug.Log("Is it moving?");
-    }
     void initialize()
     {
-        pos = new Vector3(0, 0, 0);
-        if(bgImg != null)
+        if (bgImg1 != null && bgImg2 != null)
         {
-            pos = transform.position;
+            //pos = transform.position;
+            pos1 = bgImg1.transform.position;
+            pos2 = bgImg2.transform.position;
+            resetPosX1 = -pos2.x;
+            resetPosX2 = pos1.x;
         }
-        switch(tag)
+        switch (tag)
         {
             case "TitleScreenBg":
                 break;
@@ -51,4 +45,25 @@ public class BackgroundController : MonoBehaviour
         Debug.Log(tag);
         Debug.Log(speed);
     }
+    void move()
+    {
+        if(bgImg1.transform.position.x > resetPosX1)
+        {
+            bgImg1.transform.position -= newPos;
+        }
+        else
+        {
+            bgImg1.transform.position = pos1;
+        }
+
+        if(bgImg2.transform.position.x > resetPosX2)
+        {
+            bgImg2.transform.position -= newPos;
+        }
+        else
+        {
+            bgImg2.transform.position = pos2;
+        }
+    }
+    
 }
