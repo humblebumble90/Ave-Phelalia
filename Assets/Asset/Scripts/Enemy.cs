1d72e64f57b6ad8e34234cdef2c045325bfaee69
humblebumble90 <hyngsuki@gmail.com>
2021-10-05 21:08:36 -0400

    Creating enemy scripts...

---
+++ Assets/Asset/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy
+{
+    private float hp;
+    private float speed;
+    private float fireRate;
+    private bool ifFire;
+    private bool ifBoss;
+    public Enemy(float hp, float speed, float fireRate, bool ifFire, bool ifBoss)
+    {
+        this.hp = hp;
+        this.speed = speed;
+        this.fireRate = fireRate;
+        this.ifFire = ifFire;
+        this.ifBoss = ifBoss;
+    }
+
+    public abstract void move();
+    public abstract void attack();
+    public abstract void destroy();
+}
---
+++ Assets/Asset/Scripts/Enemy1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : Enemy
+{
+    public Enemy1(float hp, float speed, float fireRate, bool ifFire, bool ifBoss):base(hp,speed,fireRate,ifFire,ifBoss)
+    {
+    }
+    public override void attack()
+    {
+    }
+
+    public override void destroy()
+    {
+    }
+
+    public override void move()
+    {
+    }
+}
